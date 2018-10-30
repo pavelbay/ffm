358688bb9eef7838d534b24a765d4f2a9f3769f9
Pavel Bayburtyan <pavel.bayburtyan@gmail.com>
2018-10-30 22:45:13 +0100

    remove comment

--- friend_for_me/lib/main.dart
+++ friend_for_me/lib/main.dart
@@ -148,11 +148,6 @@ class _MyHomePageState extends State<MyHomePage>
           ],
         ),
       ),
-//      floatingActionButton: new FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: new Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
