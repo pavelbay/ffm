82e3308ec8913b411c9c260cbd908772bbede852
Pavel Bayburtyan <pavel.bayburtyan@gmail.com>
2018-10-30 20:53:43 +0100

    display pictures

--- friend_for_me/lib/main.dart
+++ friend_for_me/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const IMAGE_ASSET_PATH = 'graphics';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Friend for me'),
+      home: new MyHomePage(title: 'Friend for me!'),
     );
   }
 }
@@ -23,8 +25,20 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
+  AnimationController _controller;
+
+  _MyHomePageState() : super() {
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.stop();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -32,27 +46,77 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget _buildIcons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: _buildImage('clouds.webp'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: _buildImage('sun.webp'),
+        )
+      ],
+    );
+  }
+
+  Widget _buildImage(String assetName) {
+    final String path = '$IMAGE_ASSET_PATH/$assetName';
+    return Image.asset(path, height: 100.0, width: 100.0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
+//      appBar: new AppBar(
+//        title: new Text(widget.title),
+//      ),
       body: new Center(
         child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            new Text(
-              'Weather',
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: new Text(
+                    'Weather',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                        color: Colors.blueGrey),
+                  ),
+                ),
+                _buildIcons(),
+              ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 30.0,
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(30.0),
+                        topRight: const Radius.circular(10.0))),
+                child: LinearProgressIndicator(
+                  value: 0.0,
+                  backgroundColor: Colors.amberAccent,
+                  valueColor:
+                      ColorTween(begin: Colors.green).animate(_controller),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+//      floatingActionButton: new FloatingActionButton(
+//        onPressed: _incrementCounter,
+//        tooltip: 'Increment',
+//        child: new Icon(Icons.add),
+//      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
